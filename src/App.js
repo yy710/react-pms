ecf4f3e0aa4be3ac120eb772635c9d944a0ce040
yy710 <171254868@qq.com>
2019-10-08 21:08:22 +0800

    second commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 //import logo from './logo.svg';
 import './App.css';
 
 import Pmboard from './components/Pmboard'
 
+let _pmList = [
+  [
+    { id: 1, auther: "张三1", attach: "PSD" },
+    { id: 2, auther: "李四1", attach: "EXCEL" },
+    { id: 3, auther: "王五1", attach: "WORD" }
+  ],
+  [
+    { id: 4, auther: "张三2", attach: "PSD" },
+    { id: 5, auther: "李四2", attach: "EXCEL" },
+    { id: 6, auther: "王五2", attach: "WORD" }
+  ],
+  [
+    { id: 7, auther: "张三3", attach: "PSD" },
+    { id: 8, auther: "李四3", attach: "EXCEL" },
+    { id: 9, auther: "王五3", attach: "WORD" }
+  ]
+];
+
 function App() {
+  function addList(n, r) {
+    //console.log("click e: ", e.target.id);
+    console.log("addList(", n, ")");
+    console.log(r);
+  }
+
   return (
     <div className="board">
-      <Pmboard color="orange" title="客户需求记录" boardid="aaa"></Pmboard>
-      <Pmboard color="blue" title="已分配任务" boardid="bbb"></Pmboard>
-      <Pmboard color="green" title="已完成任务" boardid="ccc"></Pmboard>
+      <Pmboard key="collections" color="orange" title="客户需求记录" onAddList={r => addList(1, r)} pmList={_pmList[0]}></Pmboard>
+      <Pmboard key="tasks" color="blue" title="已分配任务" onAddList={r => addList(2, r)} pmList={_pmList[1]}></Pmboard>
+      <Pmboard key="finished" color="green" title="已完成任务" onAddList={r => addList(3, r)} pmList={_pmList[2]}></Pmboard>
     </div>
   );
 }
--- src/components/Pmboard.js
+++ src/components/Pmboard.js
@@ -2,32 +2,29 @@ import React, { useState } from 'react';
 import Pmlist from './Pmlist'
 import './Pmboard.css'
 
-let _pmList = [
-    { auther: "张三", attach: "PSD" },
-    { auther: "李四", attach: "EXCEL" },
-    { auther: "王五", attach: "WORD" }
-];
-
 function Pmboard(props) {
-    let [listItems, setPmList] = useState(data2Pmlist(_pmList));
+    let pmList = props.pmList;
+    let [listItems, setListItems] = useState(data2Pmlist(pmList));
 
-    function addList(e) {
-        console.log("click e: ", e.target.id);
-        _pmList.push({ auther: "王找刘", attach: "WORD" });
-        setPmList(data2Pmlist(_pmList));
+    function AddList() {
+        const newid = pmList[pmList.length - 1].id + 1;
+        pmList.push({ id: newid, auther: "王找刘", attach: "WORD" });
+        setListItems(data2Pmlist(pmList));
+        props.onAddList(pmList);
     }
 
     return (
-        <div className="list" style={{ background: props.color }}>
+        <div className="list" style={{ background: "white" }}>
+            <div>{props.title}</div>
             {listItems}
-            <div id={props.boardid} onClick={addList}>+新增</div>
+            <div onClick={AddList}>+新增</div>
         </div>
     );
 }
 
-function data2Pmlist(_list){
+function data2Pmlist(_list) {
     const resulte = _list.map(item => {
-        return (<Pmlist key={item.auther} auther={item.auther} time={(new Date()).toLocaleString()} attach={item.attach}></Pmlist>);
+        return (<Pmlist key={item.id} auther={item.auther} time={(new Date()).toLocaleString()} attach={item.attach}></Pmlist>);
     });
     return resulte;
 }
