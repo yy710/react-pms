d7b6652c54157b9247d384464e8ae886747da1cd
yy710 <171254868@qq.com>
2019-10-23 01:03:40 +0800

    ent

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -107,6 +107,38 @@ function merge_options(obj1,obj2){
     return obj3;
 }
 
+function mergeOptions(options, defaults) {
+    for (var key in defaults) {
+        options[key] = options[key] || defaults[key];
+    }
+}
+
+/**
+* Recursively merge properties of two objects 
+*/
+function MergeRecursive(obj1, obj2) {
+
+    for (var p in obj2) {
+      try {
+        // Property in destination object set; update its value.
+        if ( obj2[p].constructor==Object ) {
+          obj1[p] = MergeRecursive(obj1[p], obj2[p]);
+  
+        } else {
+          obj1[p] = obj2[p];
+  
+        }
+  
+      } catch(e) {
+        // Property in destination object not set; create it and set its value.
+        obj1[p] = obj2[p];
+  
+      }
+    }
+  
+    return obj1;
+  }
+
 /**
  * component write by class
  * 
